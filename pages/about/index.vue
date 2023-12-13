564e9ce6d3ca18071d38e765b763b6cb1b15b968
zydemail <1054055589@qq.com>
2023-12-13 16:41:35 +0800

    scss

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
-        <h1>about</h1>
+        <h1>{{ name }}</h1>
     </div>
     <nuxt-link to="/home">home</nuxt-link>
     <nuxt-link to="/contact">Contact</nuxt-link>
 </template>
+<script setup lang="ts">
+const name = 'about'
+</script>
+<style scoped lang="scss">
+h1 {
+    color: red;
+}
+</style>
